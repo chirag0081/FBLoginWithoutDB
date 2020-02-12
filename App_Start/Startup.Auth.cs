f8fe1d787545e9a6f3cb09ca717e731e911d01e7
arrowchirag0081 <arrow.chirag@gmail.com>
2020-02-12 18:27:07 +0530

    First Commit

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.Facebook;
+
+namespace SocialLoginWithoutIdentity
+{
+    public partial class Startup
+    {
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            var cookieOptions = new CookieAuthenticationOptions
+            {
+                LoginPath = new PathString("/Account/Login")
+            };
+
+            app.UseCookieAuthentication(cookieOptions);
+
+            app.SetDefaultSignInAsAuthenticationType(cookieOptions.AuthenticationType);
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
+            {
+                ClientId = "rGaUsIUyGmqcuu194Xv9PKfmr",
+                ClientSecret = "pewTG74M9fUiAfFU5S35BinksMSg3HWJszLq2nMTI614UTzULs"
+            });
+            app.UseFacebookAuthentication(new FacebookAuthenticationOptions
+            {
+                AppId = "208623500317955",
+                AppSecret = "bb084821b9a0e5aaa13cb610db02a628"
+            });
+
+
+        }
+    }
+}
---
+++ Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialLoginWithoutIdentity.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        //
+        // POST: /Account/ExternalLogin
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExternalLogin(string provider, string returnUrl)
+        {
+            // Request a redirect to the external login provider
+            return new ChallengeResult(provider, Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl }));
+
+        }
+
+        public ActionResult ExternalLoginCallback(string returnUrl)
+        {
+            return new RedirectResult(returnUrl);
+        }
+
+        // Implementation copied from a standard MVC Project, with some stuff
+        // that relates to linking a new external login to an existing identity
+        // account removed.
+        private class ChallengeResult : HttpUnauthorizedResult
+        {
+            public ChallengeResult(string provider, string redirectUri)
+            {
+                LoginProvider = provider;
+                RedirectUri = redirectUri;
+            }
+
+            public string LoginProvider { get; set; }
+            public string RedirectUri { get; set; }
+
+            public override void ExecuteResult(ControllerContext context)
+            {
+                var properties = new AuthenticationProperties() { RedirectUri = RedirectUri };
+                context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
+            }
+        }
+    }
+}
